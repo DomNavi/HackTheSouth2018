9d1e7c46de2132fa1c2054516624d9ed71e125d4
DomasNav <navikasdomas@gmail.com>
2018-05-05 20:58:19 +0100

    Some Changes

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    //public WebCamTexture mCamera = null;
+    public GameObject plane;
+    public Text debugText;
+    //public SpriteRenderer cameraPane;
+    private WebCamTexture webcamTexture = null;
+    public SpriteRenderer backgroundSprite;
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Script has been started");
+        plane = GameObject.FindWithTag("CameraPlane");
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(Screen.width, Screen.height);
+
+        if (devices.Length > 0) {
+            webcamTexture.deviceName = devices[0].name;
+            webcamTexture.Play();
+        }
+
+        plane.GetComponent<Renderer>().material.mainTexture = webcamTexture;
+        //debugText.text = devices[0].name;
+    }
+
+    public void TakePicture() {
+        Color32[] imagePixels = webcamTexture.GetPixels32();
+        Texture2D texture = new Texture2D(webcamTexture.width, webcamTexture.height);
+        texture.SetPixels32(imagePixels);
+        texture.Apply();
+        Sprite sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        backgroundSprite.sprite = sprite;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/Painter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Painter : MonoBehaviour {
+
+    private List<Vector2> drawPoints;
+
+    void Update() {
+        if( Input.touchCount > 0) {
+            if (Input.GetTouch(0).phase == TouchPhase.Began) {
+                drawPoints = new List<Vector2>();
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Moved) {
+                // Construct a ray from the current touch coordinates
+                //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                // Create a particle if hit
+                //if (Physics.Raycast(ray)) {
+                if (!drawPoints.Contains(Input.GetTouch(0).position)) {
+                    drawPoints.Add((Input.GetTouch(0).position));
+                }
+                    //Instantiate(particle, transform.position, transform.rotation);
+                //}
+            }
+        }
+        //if(drawPoints != null)
+        //Debug.Log(drawPoints.ToArray()[drawPoints.Count - 1]);
+    }
+}
